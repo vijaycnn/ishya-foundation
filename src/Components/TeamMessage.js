e33a987b0b66328a04b9d65fb96ad01cab4310b8
Majid Equbal <majide@neuronimbus.com>
2026-07-31 18:56:12 +0530

    done UI of new team member

--- src/Components/TeamMessage.js
+++ src/Components/TeamMessage.js
@@ -2,34 +2,62 @@ import React from "react";
 import "../Styles/TeamMessage.css";
 import CoFounderImage from "../Images/cofounder.jpg";
 import ProfileImage from "../Images/Founder.png"; // Replace with actual image
-import FloralDesign1 from "../Images/FloralDesign1.png"; // Provide the PNG
+
 import FloralDesign2 from "../Images/FloralDesign2.png"; // Provide the PNG
 
 const ProfileSection = () => {
   return (
     <div className="profile-section">
-      <img src={FloralDesign1} alt="Background Design" className="background-design" loading="lazy"/>
-      
-      <div className="profile-content">
-        <div className="profile-image-wrapper">
-          <img src={ProfileImage} alt="Profile" className="profile-image" loading="lazy"/>          
-          <p className="profile-name">Shalini Gupta</p>
-          <p className="profile-designation">FOUNDER</p>
-        </div>
-        <div className="profile-text">
-          <p className="profile-description">
-            {/* My goal for ishya is to develop an organisation that combats the disparity in knowledge and poverty within the society . iam to achieve this by estabilishing a resourceful institution that promotes and focters financial autonomy for individuals. */}
-            Our goal for Ishya Foundation is to develop an organisation that combats the disparity in knowledge and poverty within society. We aim to achieve this by establishing a resourceful learning institution that promotes education and financial autonomy for individuals.
-          </p>
-        </div>
-        <div className="profile-image-wrapper">
-          <img src={CoFounderImage} alt="Profile" className="profile-image" loading="lazy"/>          
-          <p className="profile-name">Ishita Gupta</p>
-          <p className="profile-designation">Co-FOUNDER</p>
-        </div>
-      </div>
+      <div className="profile-section-container">
+        <div className="profile-content">
+          <div className="profile-image-wrapper">
+            <div className="profile-image-thumb">
+              <img
+                src={ProfileImage}
+                alt="Profile"
+                className="profile-image"
+                loading="lazy"
+              />
+            </div>
+            <div className="profile-image-text">
+              <h4>Shalini Gupta</h4>
+              <p>FOUNDER</p>
+            </div>
+          </div>
+          <div className="profile-text">
+            <p className="profile-description">
+              {/* My goal for ishya is to develop an organisation that combats the disparity in knowledge and poverty within the society . iam to achieve this by estabilishing a resourceful institution that promotes and focters financial autonomy for individuals. */}
+              Our goal for Ishya Foundation is to develop an organisation that
+              combats the disparity in knowledge and poverty within society. We
+              aim to achieve this by establishing a resourceful learning
+              institution that promotes education and financial autonomy for
+              individuals.
+            </p>
+          </div>
+          <div className="profile-image-wrapper">
+            <div className="profile-image-thumb">
+              <img
+                src={CoFounderImage}
+                alt="Profile"
+                className="profile-image"
+                loading="lazy"
+              />
+            </div>
 
-      <img src={FloralDesign2} alt="Corner Design" className="corner-design" loading="lazy"/>
+            <div className="profile-image-text">
+              <h4>Ishita Gupta</h4>
+              <p>CO-FOUNDER</p>
+            </div>
+          </div>
+        </div>
+
+        <img
+          src={FloralDesign2}
+          alt="Corner Design"
+          className="corner-design"
+          loading="lazy"
+        />
+      </div>
     </div>
   );
 };
