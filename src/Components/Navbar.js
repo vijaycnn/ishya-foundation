bafdf6c9bf81034db6d7c9cc802abb40687c0681
Majid Equbal <majide@neuronimbus.com>
2026-08-17 16:37:37 +0530

    contact number updated on header topbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import '../Styles/Navbar.css';
-import Logo from '../Images/IshyaLogo.png';
-import { FaSearch, FaHandsHelping, FaBars, FaTimes } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "../Styles/Navbar.css";
+import Logo from "../Images/IshyaLogo.png";
+import { FaSearch, FaHandsHelping, FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [openMenus, setOpenMenus] = useState({});
@@ -22,23 +22,31 @@ const Navbar = () => {
 
   const navItems = [
     {
-      label: 'About Us',
-      link: '/about-us/who-are-we',
-      menu: ['Who are we', 'Finances & Reporting','Ishya Learning Centre'],
+      label: "About Us",
+      link: "/about-us/who-are-we",
+      menu: ["Who are we", "Finances & Reporting", "Ishya Learning Centre"],
     },
     {
-      label: 'Our Programs',
-      link: '/ourprograms',
-      menu: ['All','Education', 'Health & Wellbeing', 'Community Development', 'Women Empowerment', 'Environment', 'Art & Culture'],
+      label: "Our Programs",
+      link: "/ourprograms",
+      menu: [
+        "All",
+        "Education",
+        "Health & Wellbeing",
+        "Community Development",
+        "Women Empowerment",
+        "Environment",
+        "Art & Culture",
+      ],
     },
     {
-      label: 'Media & Coverage',
-      link: '/media-&-coverage',
-      menu: ['Gallery', 'Blog', 'News', 'Newsletter'],
+      label: "Media & Coverage",
+      link: "/media-&-coverage",
+      menu: ["Gallery", "Blog", "News", "Newsletter"],
     },
     {
-      label: 'Contact Us',
-      link: '/contactus',
+      label: "Contact Us",
+      link: "/contactus",
     },
   ];
 
@@ -48,8 +56,8 @@ const Navbar = () => {
         <div className="navbar-upper">
           <ul className="navbar-left">
             <li>
-              <Link to="">
-                <span>+918527690615</span>
+              <Link to="tel:9871005650">
+                <span>+919871005650</span>
               </Link>
             </li>
           </ul>
@@ -64,13 +72,18 @@ const Navbar = () => {
         </div>
         <div className="navbar-lower">
           <div className="mobile-logo-menu">
-            { !isMobileMenuOpen &&
-            <Link to="/">
-              <img src={Logo} alt="Logo" className="logo" style={{ width: '70px' }} />
-            </Link>
-            }
-            <button 
-              className="mobile-menu-btn" 
+            {!isMobileMenuOpen && (
+              <Link to="/">
+                <img
+                  src={Logo}
+                  alt="Logo"
+                  className="logo"
+                  style={{ width: "70px" }}
+                />
+              </Link>
+            )}
+            <button
+              className="mobile-menu-btn"
               onClick={toggleMobileMenu}
               aria-label={isMobileMenuOpen ? "Close menu" : "Open menu"}
             >
@@ -78,28 +91,41 @@ const Navbar = () => {
             </button>
           </div>
 
-          <ul className={`menu-items ${isMobileMenuOpen ? 'show-mobile-menu' : ''}`}>
-            {  
+          <ul
+            className={`menu-items ${isMobileMenuOpen ? "show-mobile-menu" : ""}`}
+          >
+            {
               <li className="desktop-logo">
-              <Link to="/">
-                <img src={Logo} alt="Logo" className="logo" style={{ width: '70px' }} />
-              </Link>
-            </li>
+                <Link to="/">
+                  <img
+                    src={Logo}
+                    alt="Logo"
+                    className="logo"
+                    style={{ width: "70px" }}
+                  />
+                </Link>
+              </li>
             }
             {navItems.map((item) => (
               <li
                 key={item.label}
-                onMouseEnter={item.menu ? () => handleMouseEnter(item.label) : null}
-                onMouseLeave={item.menu ? () => handleMouseLeave(item.label) : null}
-                className={item.menu ? 'has-menu' : ''}
+                onMouseEnter={
+                  item.menu ? () => handleMouseEnter(item.label) : null
+                }
+                onMouseLeave={
+                  item.menu ? () => handleMouseLeave(item.label) : null
+                }
+                className={item.menu ? "has-menu" : ""}
               >
-                <Link to={item.link} onClick={() => setIsMobileMenuOpen(false)}>{item.label}</Link>
+                <Link to={item.link} onClick={() => setIsMobileMenuOpen(false)}>
+                  {item.label}
+                </Link>
                 {item.menu && openMenus[item.label] && (
                   <ul className="dropdown-menu">
                     {item.menu.map((menuItem) => (
                       <li key={menuItem}>
-                        <Link 
-                          to={`/${item.label.toLowerCase().replace(/ /g, '-')}/${menuItem.toLowerCase().replace(/ /g, '-')}`}
+                        <Link
+                          to={`/${item.label.toLowerCase().replace(/ /g, "-")}/${menuItem.toLowerCase().replace(/ /g, "-")}`}
                           onClick={() => setIsMobileMenuOpen(false)}
                         >
                           {menuItem}
@@ -110,16 +136,15 @@ const Navbar = () => {
                 )}
               </li>
             ))}
-            
+
             <li className="navbar-support">
               <Link to="/donate" onClick={() => setIsMobileMenuOpen(false)}>
                 <button className="support-btn">
-                  <FaHandsHelping style={{ marginRight: '10px' }} />
+                  <FaHandsHelping style={{ marginRight: "10px" }} />
                   <span>Support a Cause</span>
                 </button>
               </Link>
             </li>
-            
           </ul>
         </div>
       </nav>
