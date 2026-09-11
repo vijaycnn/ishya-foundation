0d63b8e6396c4caecfad4470fc101109533277de
ajit kumar <50000875@mahindra.com>
2026-09-11 19:10:45 +0530

    add vite.config file

---
+++ react_admin/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base : '/admin/',
+  plugins: [react()],
+  server: {
+    port: 5174, // 👈 change this to your custom port
+    strictPort: true,
+    // allowedHosts: ['uat.url.com']
+  },
+})
